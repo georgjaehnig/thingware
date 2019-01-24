1b2d87576766efaa32b75f7d88f3a5493b523b9f
Georg Jaehnig <georg@jaehnig.org>
2019-01-24 20:01:38 +0100

    Working

---
+++ index.js
@@ -0,0 +1,47 @@
+var fs = require('fs');
+var lines = fs.readFileSync('lists/unq.2.txt').toString().split("\n");
+
+function pick_titles(randoms) {
+  
+	var picked_lines = [];
+
+	for (random of randoms) {
+	  picked_lines.push(lines[random]);
+	}
+
+  console.log('Things / Wikipedia articles to remember:')
+  console.log('');
+  
+  var password = '';
+  for (line of picked_lines) {
+    var parts = line.split(" ");
+  	console.log("\thttps://en.wikipedia.org/wiki/" + parts[1] + "_" + parts[2]);
+    password += parts[0].toLowerCase(); 
+  }
+  console.log('');
+  console.log('Your password, based on the first and last letters of every word:')
+  console.log('');
+  console.log("\t" + password);
+  console.log('');
+  console.log('Blind entropy: 56 bits (12 lowercase chars)');
+  console.log('Seen entropy:  53 bits (3 article titles out 212686)');
+  console.log('');
+  console.log('Feel free to write out one or several words, to increase the length and thus the blind entropy.')
+}
+
+
+function main() {
+  
+  var args = process.argv.slice(2);
+  
+  if (args.length != 3) {
+    console.log("Usage: node index.js random_number1 random_number2 random_number3");
+    console.log("The random numbers must be between 1 and " + lines.length + ".");
+    console.log("You can generate them with Google: https://www.google.com/search?q=random%20number%20between%200%20and%20" + lines.length + " &ie=utf-8");
+		return;
+  }
+
+	pick_titles(args);
+}
+
+main();
